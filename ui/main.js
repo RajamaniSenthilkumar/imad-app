9287c12bebfce1667ef122012a1659779325b45d
RajamaniSenthilkumar <30681784+RajamaniSenthilkumar@users.noreply.github.com>
2017-08-14 10:53:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML = 'New Value';
 //Move the image
 var img = document.getElementById('madi');
 img.onclick = function (){
-  var interval = setInterval(moveLeft, 100);
+  var interval = setInterval(moveLeft,300);
   img.style.marginLeft = '500px'  ;
 };
