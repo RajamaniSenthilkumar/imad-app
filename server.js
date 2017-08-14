c2ca9eb7e9e3871ed3068fc1c759006a4c7d5225
RajamaniSenthilkumar <30681784+RajamaniSenthilkumar@users.noreply.github.com>
2017-08-14 18:32:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+//app.get('/ui/main.js', function (req, res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+//});
 
 app.get('/ui/Rajamani.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Rajamani.png'));
