5ff604951d1f3e0517b272d6db32ac40208a9c6c
RajamaniSenthilkumar <30681784+RajamaniSenthilkumar@users.noreply.github.com>
2017-08-14 17:25:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 var marginLeft =0;
 function moveRight(){
-    marginLeft = marginLeft =100;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function (){
