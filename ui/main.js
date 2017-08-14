5b2ccfd3f33375f0d14cf7a837d1a576fd214129
RajamaniSenthilkumar <30681784+RajamaniSenthilkumar@users.noreply.github.com>
2017-08-14 18:47:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,6 @@ button.onclick = function () {
         }
     };
     
-    request.open('GET','http://http://senrasu.imad.hasura-app.io/counter',true);
+    request.open('GET','http://senrasu.imad.hasura-app.io/counter',true);
     request.send(null);
 };
